536b11e409a01329be9b1cc59cfca9c30c7fe34e
minjeong kim <6alswjd6@gmail.com>
2021-06-22 15:01:34 +0900

    signup field

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    nickname = models.CharField(max_length=100)
+    university = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    studentnumber = models.IntegerField(default = 0)
+    major = models.CharField(max_length=50)
+    entrancetype = models.CharField(max_length=50)
+    highschool = models.CharField(max_length=50)
+

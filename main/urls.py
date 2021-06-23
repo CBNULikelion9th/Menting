8babec7c1c1ef282803706bafc86f66cb6d9f0fd
maatanyy <76435279+maatanyy@users.noreply.github.com>
2021-06-23 16:45:39 +0900

    community 1차 (#5)
    * page
    
    * static 추가
    
    * community 1차

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('community/',views.community_page, name='community_page'),
+    path('community/new/',views.community_new, name='community_new'),
+    path('community/<int:post_id>/',views.community_detail, name="community_detail"),
     path('notice/',views.notice_page, name='notice_page'),
     path('search/',views.search_page, name='search_page'),
 ]

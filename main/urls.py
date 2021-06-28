f854631f330d124eb6244ebc9e8980c39fe0db6d
maatanyy <76435279+maatanyy@users.noreply.github.com>
2021-06-28 22:35:05 +0900

    community,notice,search 틀 만들기 (수정전) (#10)
    * page
    
    * static 추가
    
    * community 1차
    
    * community 2차, notice 1차, search 틀만

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,11 @@ urlpatterns = [
     path('community/',views.community_page, name='community_page'),
     path('community/new/',views.community_new, name='community_new'),
     path('community/<int:post_id>/',views.community_detail, name="community_detail"),
+    path('community/<int:post_id>/delete/',views.community_delete, name="community_delete"),
+    path('community/<int:post_id>/comment/',views.community_comment, name="community_comment"),
     path('notice/',views.notice_page, name='notice_page'),
+    path('notice/new/',views.notice_new, name='notice_new'),
+    path('notice/<int:post_id>/',views.notice_detail, name="notice_detail"),
     path('search/',views.search_page, name='search_page'),
     path('',views.home, name='home'),
 ]

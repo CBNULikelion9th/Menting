7ce5692391d221d0fb612a26a35301b7a1db3120
maatanyy <maatanyy@naver.com>
2021-06-28 22:33:42 +0900

    community 2차, notice 1차, search 틀만

--- main/forms.py
+++ main/forms.py
@@ -1,9 +1,21 @@
 from django import forms
 from django.db.models import fields
-from .models import Post
+from .models import Post, Comment, Post2
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
+        fields = ['title','content']
+
+class PostForm2(forms.ModelForm):   
+
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+class PostForm3(forms.ModelForm):
+
+    class Meta:
+        model = Post2
         fields = ['title','content']

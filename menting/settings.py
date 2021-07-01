602bec59d23dbee16f92097143d7aab3a975c4b4
banhogu <77983156+banhogu@users.noreply.github.com>
2021-07-01 21:31:26 +0900

    upload

--- menting/settings.py
+++ menting/settings.py
@@ -126,9 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+#static 경로 변경
 STATIC_URL = '/static/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'accounts/certificates')
+# -->
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

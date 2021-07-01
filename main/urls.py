3ef33ec638b170b85d4f067c1d2b321e24f2dba7
maatanyy <maatanyy@naver.com>
2021-07-01 19:58:03 +0900

    커뮤니티 댓글 삭제기능 추가

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('community/<int:post_id>/',views.community_detail, name="community_detail"),
     path('community/<int:post_id>/delete/',views.community_delete, name="community_delete"),
     path('community/<int:post_id>/comment/',views.community_comment, name="community_comment"),
+    path('community/<int:post_id>/comment/<int:comment_id>/delete/',views.comment_delete, name="comment_delete"),
     path('community/<int:post_id>/edit/',views.community_edit, name="community_edit"),
     path('notice/',views.notice_page, name='notice_page'),
     path('notice/new/',views.notice_new, name='notice_new'),

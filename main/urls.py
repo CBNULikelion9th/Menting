a6560e6337642c2c563089a85242f1617c08a8b4
maatanyy <76435279+maatanyy@users.noreply.github.com>
2021-07-01 20:29:18 +0900

    커뮤니티 댓글 편집기능 수정 (#26)
    * page
    
    * static 추가
    
    * community 1차
    
    * community 2차, notice 1차, search 틀만
    
    * community notice 조금 수정
    
    * 승인 받은 멘토 리스트 구현
    
    * 운영자만 공지사항 글 추가, 삭제가능하게 수정
    
    * community 로그인한 아이디만 글 수정, 삭제 추가
    
    * d
    
    * 커뮤니티 댓글 삭제기능 추가
    
    * community 댓글 수정 기능 추가

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('community/<int:post_id>/delete/',views.community_delete, name="community_delete"),
     path('community/<int:post_id>/comment/',views.community_comment, name="community_comment"),
     path('community/<int:post_id>/comment/<int:comment_id>/delete/',views.comment_delete, name="comment_delete"),
+    path('community/<int:post_id>/comment/<int:comment_id>/edit/',views.comment_edit, name="comment_edit"),
     path('community/<int:post_id>/edit/',views.community_edit, name="community_edit"),
     path('notice/',views.notice_page, name='notice_page'),
     path('notice/new/',views.notice_new, name='notice_new'),

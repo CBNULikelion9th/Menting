553cd294086a615ed63cfcb720cf69e2e2783ede
minjeong kim <6alswjd6@gmail.com>
2021-07-02 22:02:15 +0900

    url 수정

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('community/',views.community_page, name='community_page'),
+    path('main/community/',views.community_page, name='community_page'),
     path('community/new/',views.community_new, name='community_new'),
     path('community/<int:post_id>/',views.community_detail, name="community_detail"),
     path('community/<int:post_id>/delete/',views.community_delete, name="community_delete"),
@@ -10,7 +10,7 @@ urlpatterns = [
     path('community/<int:post_id>/comment/<int:comment_id>/delete/',views.comment_delete, name="comment_delete"),
     path('community/<int:post_id>/comment/<int:comment_id>/edit/',views.comment_edit, name="comment_edit"),
     path('community/<int:post_id>/edit/',views.community_edit, name="community_edit"),
-    path('notice/',views.notice_page, name='notice_page'),
+    path('main/notice/',views.notice_page, name='notice_page'),
     path('notice/new/',views.notice_new, name='notice_new'),
     path('notice/<int:post_id>/',views.notice_detail, name="notice_detail"),
     path('notice/<int:post_id>/delete/',views.notice_delete, name="notice_delete"),

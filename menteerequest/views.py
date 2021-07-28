5ee67c811fdc50445a118e1a46f03e45080a3f12
minjeong kim <6alswjd6@gmail.com>
2021-07-29 00:37:38 +0900

    평점창

--- menteerequest/views.py
+++ menteerequest/views.py
@@ -61,7 +61,7 @@ def request_detail(request,post_id):  # 디테일 페이지
 
 
 def request_response(request, post_id):
-    post = get_object_or_404(Mentee_request, id=post_id) #Post를 인자로 받고 get()함수로 넘김? 객체가 존재하지 않으면 오류 발생
+    post = get_object_or_404(Mentee_request, id=post_id) 
     if request.method == "POST":
         form = ResponseForm(request.POST)
         if form.is_valid():
@@ -76,7 +76,8 @@ def request_response(request, post_id):
 
 
 def request_response_reject(request, post_id):
-    post = get_object_or_404(Mentee_request, id=post_id) #Post를 인자로 받고 get()함수로 넘김? 객체가 존재하지 않으면 오류 발생
+    post = get_object_or_404(Mentee_request, id=post_id) 
+    
     if request.method == "POST":
         form = ResponseForm(request.POST)
         if form.is_valid():

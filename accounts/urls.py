0741c8fc29bb04e81d91c66783be54ceebf923bf
minjeong kim <6alswjd6@gmail.com>
2021-08-05 15:18:59 +0900

    회원탈퇴

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('password_reset_done/', views.UserPasswordResetDoneView.as_view(), name="password_reset_done"),
     path('password_reset_confirm/<uidb64>/<token>/', views.UserPasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     path('password_reset_complete/', views.UserPasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path("user_delete",views.User_delete, name = 'user_delete'),
 ]
